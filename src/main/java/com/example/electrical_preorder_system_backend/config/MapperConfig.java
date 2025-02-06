d73264cf2e917044f97aae320b14804ea1003b25
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-02-06 14:16:42 +0700

    config: add mapper configuration

---
+++ src/main/java/com/example/electrical_preorder_system_backend/config/MapperConfig.java
@@ -0,0 +1,13 @@
+package com.example.electrical_preorder_system_backend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
