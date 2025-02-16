59676a16d0c090a78286248037430b21db7c7774
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-02-16 21:21:06 +0700

    fix: fix bug api response

--- src/main/java/com/example/electrical_preorder_system_backend/dto/response/ApiResponse.java
+++ src/main/java/com/example/electrical_preorder_system_backend/dto/response/ApiResponse.java
@@ -3,11 +3,9 @@ package com.example.electrical_preorder_system_backend.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@RequiredArgsConstructor
 @NoArgsConstructor
 public class ApiResponse {
     private String message;
