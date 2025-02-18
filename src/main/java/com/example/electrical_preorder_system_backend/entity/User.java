3f4514376ea77a1417cc99b59161361ee54f1976
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-02-18 15:33:17 +0700

    fix: change table name to singular noun

--- src/main/java/com/example/electrical_preorder_system_backend/entity/User.java
+++ src/main/java/com/example/electrical_preorder_system_backend/entity/User.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-@Table(name = "users", indexes = {
+@Table(name = "\"user\"", indexes = {
         @Index(name = "idx_user_name", columnList = "username"),
         @Index(name = "idx_email", columnList = "email"),
         @Index(name = "idx_phone_number", columnList = "phone_number")
