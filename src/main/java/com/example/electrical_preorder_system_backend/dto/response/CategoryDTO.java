2086c258974817450b3af1bdd56e838fa08d851c
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-02-25 23:33:17 +0700

    feat(category): add CRUD category

--- src/main/java/com/example/electrical_preorder_system_backend/dto/response/CategoryDTO.java
+++ src/main/java/com/example/electrical_preorder_system_backend/dto/response/CategoryDTO.java
@@ -1,6 +1,5 @@
 package com.example.electrical_preorder_system_backend.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.UUID;
@@ -8,6 +7,5 @@ import java.util.UUID;
 @Data
 public class CategoryDTO {
     private UUID id;
-    @NotBlank
     private String name;
 }
