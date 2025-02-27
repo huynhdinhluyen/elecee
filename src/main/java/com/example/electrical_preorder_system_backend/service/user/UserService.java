332896c528fa8903b2caef860a8eff72ce1efc77
huynhdinhluyen <huynhdinhluyen@gmail.com>
2025-02-27 09:19:17 +0700

    resolve conflict UserService.java

--- src/main/java/com/example/electrical_preorder_system_backend/service/user/UserService.java
+++ src/main/java/com/example/electrical_preorder_system_backend/service/user/UserService.java
@@ -1,23 +1,24 @@
 package com.example.electrical_preorder_system_backend.service.user;
 
+import com.example.electrical_preorder_system_backend.config.client.GoogleIdentityClient;
+import com.example.electrical_preorder_system_backend.config.client.GoogleUserClient;
 import com.example.electrical_preorder_system_backend.config.jwt.JwtUtils;
-import com.example.electrical_preorder_system_backend.config.utils.UserDetailsImpl;
-import com.example.electrical_preorder_system_backend.dto.request.UserLoginRequest;
+import com.example.electrical_preorder_system_backend.dto.request.ExchangeTokenRequest;
+import com.example.electrical_preorder_system_backend.dto.request.UpdateUserRequest;
 import com.example.electrical_preorder_system_backend.dto.request.UserSignUpRequest;
+import com.example.electrical_preorder_system_backend.dto.response.AuthenticationResponse;
+import com.example.electrical_preorder_system_backend.dto.response.UserDTO;
 import com.example.electrical_preorder_system_backend.entity.User;
 import com.example.electrical_preorder_system_backend.enums.UserRole;
 import com.example.electrical_preorder_system_backend.enums.UserStatus;
+import com.example.electrical_preorder_system_backend.mapper.UserMapper;
 import com.example.electrical_preorder_system_backend.repository.UserRepository;
-import com.example.electrical_preorder_system_backend.service.email.EmailService;
-import jakarta.mail.MessagingException;
+import com.example.electrical_preorder_system_backend.util.Validator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionSystemException;
@@ -25,39 +26,40 @@ import org.springframework.transaction.TransactionSystemException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
 
-    private final AuthenticationManager authenticationManager;
-
-    @Autowired
     private final JwtUtils jwtUtils;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private EmailService emailService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final GoogleIdentityClient googleIdentityClient;
+    private final GoogleUserClient googleUserClient;
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
 
     @Override
-    public User signUp(UserSignUpRequest userSignInRequest) throws MessagingException {
-        try {
-            if (userRepository.existsByUsername(userSignInRequest.getUsername())) {
-                throw new RuntimeException("SignUp failed: Username already exists");
-            } else if (userRepository.existsByEmail(userSignInRequest.getEmail())) {
-                throw new RuntimeException("SignUp failed: Email already exists");
-            } else if (userRepository.existsByPhoneNumber(userSignInRequest.getPhoneNumber())) {
-                throw new RuntimeException("SignUp failed: Phone number already exists");
-            }
-            String role = userSignInRequest.getRole();
-            if (!isValidRole(role)) {
-                throw new RuntimeException("SignUp failed: Invalid role");
-            }
 
+    public UserDTO signUp(UserSignUpRequest userSignInRequest) {
+        if (userRepository.existsByUsername(userSignInRequest.getUsername())) {
+            throw new RuntimeException("SignUp failed: Username already exists");
+        } else if (userRepository.existsByEmail(userSignInRequest.getEmail())) {
+            throw new RuntimeException("SignUp failed: Email already exists");
+        } else if (userRepository.existsByPhoneNumber(userSignInRequest.getPhoneNumber())) {
+            throw new RuntimeException("SignUp failed: Phone number already exists");
+        }
+        String role = userSignInRequest.getRole();
+        if (!isValidRole(role) || !role.equals(UserRole.ROLE_STAFF.name())) {
+            throw new RuntimeException("SignUp failed: Invalid role");
+        }
+        try {
             User user = User.builder()
                     .username(userSignInRequest.getUsername())
                     .password(passwordEncoder.encode(userSignInRequest.getPassword()))
@@ -65,15 +67,16 @@ public class UserService implements IUserService {
                     .email(userSignInRequest.getEmail())
                     .phoneNumber(userSignInRequest.getPhoneNumber())
                     .role(UserRole.valueOf(role))
-                    .isVerified(true)
+                    .isVerified(false)
                     .status(userSignInRequest.isActive() ? UserStatus.ACTIVE : UserStatus.INACTIVE)
                     .createdAt(LocalDateTime.now())
                     .updatedAt(LocalDateTime.now())
                     .build();
-            String jwtToken = jwtUtils.generateToken(user, "normal");
+            userRepository.save(user);
+            String jwtToken = jwtUtils.generateTokenFromUsername(user.getUsername());
             user.setToken(jwtToken);
             user.setTokenExpires(LocalDateTime.ofInstant(jwtUtils.getExpDateFromToken(jwtToken).toInstant(), ZoneId.systemDefault()));
-            return userRepository.save(user);
+            return UserMapper.toUserDTO(userRepository.save(user));
         } catch (TransactionSystemException ex) {
             log.error("Transaction failed: ", ex);
             throw new RuntimeException("Sign-up failed due to transaction error", ex);
@@ -81,59 +84,41 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public String googeLogin(UserLoginRequest userLoginRequest) throws MessagingException {
-        Optional<User> userOptional;
-        //login with Google
-        if (!userLoginRequest.getGoogleAccountId().isEmpty()) {
-            userOptional = userRepository.findByGoogleAccountId(userLoginRequest.getGoogleAccountId());
-            String jwtToken = "";
-            if (userOptional.isEmpty()) {
-                //register user
-                User newUser = User.builder()
-                        .googleAccountId(userLoginRequest.getGoogleAccountId())
-                        .username(userLoginRequest.getUsername())
-                        .fullname(userLoginRequest.getFullName())
-                        .email(userLoginRequest.getUsername())
-                        .role(UserRole.ROLE_CUSTOMER)
-                        .isVerified(false)
-                        .status(UserStatus.INACTIVE)
-                        .createdAt(LocalDateTime.now())
-                        .updatedAt(LocalDateTime.now())
-                        .build();
+    public AuthenticationResponse googleLogin(String code) {
+        try {
 
-                //Send email verification
-                emailService.sendEmail(
-                        userLoginRequest.getUsername(),
-                        emailService.subjectRegister(),
-                        emailService.bodyRegister(
-                                userLoginRequest.getUsername(),
-                                userLoginRequest.getUsername()
-                        )
-                );
-                jwtToken = jwtUtils.generateToken(newUser, "google");
-                newUser.setToken(jwtToken);
-                newUser.setTokenExpires(LocalDateTime.ofInstant(jwtUtils.getExpDateFromToken(jwtToken).toInstant(), ZoneId.systemDefault()));
-                userRepository.save(newUser);
-            } else {
-                User user = userOptional.get();
-                jwtToken = jwtUtils.generateToken(user, "google");
+            var response = googleIdentityClient.exchangeToken(ExchangeTokenRequest.builder()
+                    .code(code)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .redirectUri(redirectUri)
+                    .grantType("authorization_code")
+                    .build());
+            var userInfo = googleUserClient.getUserInfo("json", response.getAccessToken());
+            log.info("User info: {}", userInfo);
+            User user = userRepository.findByUsername(userInfo.getEmail())
+                    .orElse(null);
+            if (user == null) {
+                user = new User();
+                user.setUsername(userInfo.getEmail());
+                user.setPassword(passwordEncoder.encode(UUID.randomUUID().toString()));
+                user.setFullname(userInfo.getName());
+                user.setEmail(userInfo.getEmail());
+                user.setPhoneNumber("");
+                user.setRole(UserRole.ROLE_CUSTOMER);
+                user.setVerified(true);
+                user.setStatus(UserStatus.ACTIVE);
+                userRepository.save(user);
             }
-            log.info("Login with google - token: {}", jwtToken);
-            return jwtToken;
-        } else {
-            // Login with username and password
-            log.info("Login with username and password request: {}", userLoginRequest);
-            try {
-                Authentication authentication = authenticationManager
-                        .authenticate(new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword()));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-                UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-                return jwtUtils.generateToken(userDetails, "normal");
-            } catch (Exception e) {
-                throw new BadCredentialsException("Invalid username or password");
-            }
+            String token = jwtUtils.generateTokenFromUsername(user.getUsername());
+            user.setToken(token);
+            user.setTokenExpires(LocalDateTime.ofInstant(jwtUtils.getExpDateFromToken(token).toInstant(), ZoneId.systemDefault()));
+            userRepository.save(user);
+            return new AuthenticationResponse(token);
+        } catch (Exception e) {
+//            e.printStackTrace();
+            throw new RuntimeException("Google login failed", e);
         }
-
     }
 
     private boolean isValidRole(String role) {
@@ -145,40 +130,107 @@ public class UserService implements IUserService {
         return false;
     }
 
-    public String subjectRegister() {
-        return "Verify your email";
-    }
-
-    @Override
-    public Boolean isValidGoogleId(UserLoginRequest userLoginRequest) {
-        return true;
-    }
-
     @Override
     public void verifyEmail(String token) {
         Date expDate = jwtUtils.getExpDateFromToken(token);
-
+        String username = jwtUtils.getSubjectFromToken(token);
         // Check if the token is not expired
+
         if (!expDate.before(new Date())) {
-            String email = jwtUtils.getSubjectFromToken(token);
-            if (email != null) {
-                //Activate user account and set verified to true
-                Optional<User> userOptional = userRepository.findByEmail(email);
-                if (userOptional.isPresent()) {
-                    User user = userOptional.get();
-                    user.setStatus(UserStatus.ACTIVE);
-                    user.setVerified(true);
-                    userRepository.save(user);
-                    log.info("User with email {} verified", email);
-                } else {
-                    throw new RuntimeException("User not found");
-                }
-            } else {
-                throw new RuntimeException("Invalid token");
-            }
+            User user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            user.setVerified(true);
+            userRepository.save(user);
         } else {
             throw new RuntimeException("Token expired");
         }
+    }
+
+    @Override
+    public void update(UUID id, UpdateUserRequest updateUserRequest) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        if (user.getStatus().equals(UserStatus.INACTIVE)) {
+            throw new IllegalArgumentException("User is inactive, cannot update");
+        }
+        if (user.getStatus().equals(UserStatus.BANNED)) {
+            throw new IllegalArgumentException("User is banned, cannot update");
+        }
+        updateBasicInfo(user, updateUserRequest);
+        if (updateUserRequest.getCurrentPassword() != null && !updateUserRequest.getCurrentPassword().isEmpty()
+                && updateUserRequest.getNewPassword() != null && !updateUserRequest.getNewPassword().isEmpty()) {
+            updatePassword(user, updateUserRequest);
+        }
+    }
+
+    private void updateBasicInfo(User user, UpdateUserRequest updateUserRequest) {
+        if (updateUserRequest.getFullname() != null && !updateUserRequest.getFullname().isEmpty()) {
+            if (Validator.isValidFullname(updateUserRequest.getFullname())) {
+                //OK
+                user.setFullname(updateUserRequest.getFullname());
+            } else {
+                throw new IllegalArgumentException("Invalid fullname");
+            }
+        }
+        if (updateUserRequest.getAddress() != null && !updateUserRequest.getAddress().isEmpty()) {
+            if (Validator.isValidAddress(updateUserRequest.getAddress())) {
+                //OK
+                user.setAddress(updateUserRequest.getAddress());
+            } else {
+                throw new IllegalArgumentException("Invalid address");
+            }
+        }
+        if (updateUserRequest.getPhoneNumber() != null && !updateUserRequest.getPhoneNumber().isEmpty()) {
+            if (userRepository.existsByPhoneNumber(updateUserRequest.getPhoneNumber())) {
+                throw new IllegalArgumentException("Phone number already exists");
+            }
+            if (Validator.isValidPhoneNumber(updateUserRequest.getPhoneNumber())) {
+                //OK
+                user.setPhoneNumber(updateUserRequest.getPhoneNumber());
+            } else {
+                throw new IllegalArgumentException("Invalid phone number");
+            }
+        }
+    }
+
+    private void updatePassword(User user, UpdateUserRequest updateUserRequest) {
+        if (!passwordEncoder.matches(updateUserRequest.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("Invalid current password");
+        }
+        if (!Validator.isValidPassword(updateUserRequest.getNewPassword())) {
+            throw new IllegalArgumentException("Invalid new password");
+        }
+        user.setPassword(passwordEncoder.encode(updateUserRequest.getNewPassword()));
+        userRepository.save(user);
+    }
+
+    public void delete(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        if (user.getStatus().equals(UserStatus.INACTIVE)) {
+            throw new IllegalArgumentException("User is inactive, cannot delete");
+        }
+        if (user.getStatus().equals(UserStatus.BANNED)) {
+            throw new IllegalArgumentException("User is banned, cannot delete");
+        }
+        user.setStatus(UserStatus.INACTIVE);
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserDTO getById(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        if (user.getStatus().equals(UserStatus.INACTIVE)) {
+            throw new IllegalArgumentException("User is inactive");
+        }
+        return UserMapper.toUserDTO(user);
+    }
+
+    @Override
+    public Page<User> getUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
 
