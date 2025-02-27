9a32278b208d96fcd929a16ec47a7b3a080c47d1
huynhdinhluyen <huynhdinhluyen@gmail.com>
2025-02-27 09:18:44 +0700

    resolve conflict IUserService.java

--- src/main/java/com/example/electrical_preorder_system_backend/service/user/IUserService.java
+++ src/main/java/com/example/electrical_preorder_system_backend/service/user/IUserService.java
@@ -1,18 +1,29 @@
 package com.example.electrical_preorder_system_backend.service.user;
 
-import com.example.electrical_preorder_system_backend.dto.request.UserLoginRequest;
+import com.example.electrical_preorder_system_backend.dto.request.UpdateUserRequest;
 import com.example.electrical_preorder_system_backend.dto.request.UserSignUpRequest;
+import com.example.electrical_preorder_system_backend.dto.response.AuthenticationResponse;
+import com.example.electrical_preorder_system_backend.dto.response.UserDTO;
 import com.example.electrical_preorder_system_backend.entity.User;
 import jakarta.mail.MessagingException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface IUserService {
 
-    User signUp(UserSignUpRequest userSignInRequest) throws MessagingException;
+    UserDTO signUp(UserSignUpRequest userSignInRequest) throws MessagingException;
 
-    String googeLogin(UserLoginRequest userLoginRequest) throws MessagingException;
-
-    Boolean isValidGoogleId(UserLoginRequest userLoginRequest);
+    AuthenticationResponse googleLogin(String code) throws MessagingException;
 
     void verifyEmail(String token);
 
+    void update(UUID id, UpdateUserRequest updateUserRequest);
+
+    void delete(UUID id);
+
+    UserDTO getById(UUID id);
+
+    Page<User> getUsers(Pageable pageable);
 }
