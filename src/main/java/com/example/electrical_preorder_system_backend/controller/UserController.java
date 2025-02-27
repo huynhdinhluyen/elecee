1821d5094c9e48ab0c075f9bf998f9b26b77bfa6
huynhdinhluyen <huynhdinhluyen@gmail.com>
2025-02-27 09:12:18 +0700

    resolve conflict UserController.java

--- src/main/java/com/example/electrical_preorder_system_backend/controller/UserController.java
+++ src/main/java/com/example/electrical_preorder_system_backend/controller/UserController.java
@@ -1,21 +1,25 @@
 package com.example.electrical_preorder_system_backend.controller;
 
 import com.example.electrical_preorder_system_backend.dto.request.EmailVerificationRequest;
+import com.example.electrical_preorder_system_backend.dto.request.UpdateUserRequest;
 import com.example.electrical_preorder_system_backend.dto.request.UserSignUpRequest;
-import com.example.electrical_preorder_system_backend.entity.User;
+import com.example.electrical_preorder_system_backend.dto.response.ApiResponse;
+import com.example.electrical_preorder_system_backend.dto.response.UserDTO;
 import com.example.electrical_preorder_system_backend.service.user.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
@@ -24,19 +28,11 @@ public class UserController {
 
     @PostMapping("/sign-up")
     @SecurityRequirement(name = "Bearer Authentication")
-    public ResponseEntity<?> signUp(
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<UserDTO> signUp(
             @NonNull @RequestBody UserSignUpRequest userSignUpRequest
     ) {
-        try {
-            User user = userService.signUp(userSignUpRequest);
-            if (user == null) {
-                return ResponseEntity.badRequest().body("User signed up failed");
-            } else {
-                return ResponseEntity.ok("User signed up successfully");
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok(userService.signUp(userSignUpRequest));
     }
 
     @PostMapping("/verify")
@@ -50,5 +46,44 @@ public class UserController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/{id}")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<UserDTO> getById(
+            @NonNull @PathVariable UUID id
+    ) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @GetMapping()
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ApiResponse> getUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(new ApiResponse("Users retrieved successfully", userService.getUsers(pageable)));
+    }
+
+    @PutMapping("/{id}")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<ApiResponse> update(
+            @NonNull @PathVariable UUID id,
+            @NonNull @RequestBody UpdateUserRequest updateUserRequest
+    ) {
+        userService.update(id, updateUserRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ApiResponse> delete(
+            @NonNull @PathVariable UUID id
+    ) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
