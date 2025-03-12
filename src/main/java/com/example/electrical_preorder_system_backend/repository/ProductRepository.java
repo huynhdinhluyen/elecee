c8257352bd3b26e96b35828edf059012aacaae86
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-03-12 20:17:29 +0700

    refactor(product): remove unused functions

--- src/main/java/com/example/electrical_preorder_system_backend/repository/ProductRepository.java
+++ src/main/java/com/example/electrical_preorder_system_backend/repository/ProductRepository.java
@@ -16,8 +16,6 @@ public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpec
 
     boolean existsBySlug(String slug);
 
-    Product findByProductCode(String productCode);
-
     @Query(value = "SELECT * " +
             "FROM product " +
             "WHERE is_deleted = false " +
@@ -30,71 +28,6 @@ public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpec
                     "WHERE is_deleted = false",
             nativeQuery = true)
     Page<Product> findActiveProductsSorted(Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM product " +
-            "WHERE is_deleted = false AND category_id " +
-            "IN (SELECT id " +
-            "FROM category " +
-            "WHERE name = ?1) " +
-            "ORDER BY CASE WHEN position = 0 THEN 1 ELSE 0 END, position ASC, created_at ASC",
-            countQuery = "SELECT count(*) " +
-                    "FROM product " +
-                    "WHERE is_deleted = false AND category_id " +
-                    "IN (SELECT id " +
-                    "FROM category " +
-                    "WHERE name = ?1)",
-            nativeQuery = true)
-    Page<Product> findByCategory_NameAndIsDeletedFalse(String categoryName, Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM product " +
-            "WHERE is_deleted = false AND LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-            "ORDER BY CASE WHEN position = 0 THEN 1 ELSE 0 END, position ASC, created_at ASC",
-            countQuery = "SELECT count(*) " +
-                    "FROM product " +
-                    "WHERE is_deleted = false AND LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%'))",
-            nativeQuery = true)
-    Page<Product> findByNameContainingIgnoreCaseAndIsDeletedFalse(String name, Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM product " +
-            "WHERE is_deleted = false " +
-            "AND (LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-            "     OR LOWER(product_code) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-            "     OR LOWER(description) LIKE LOWER(CONCAT('%', ?1, '%')) ) " +
-            "ORDER BY CASE WHEN position = 0 THEN 1 ELSE 0 END, position ASC, created_at ASC",
-            countQuery = "SELECT count(*) " +
-                    "FROM product " +
-                    "WHERE is_deleted = false " +
-                    "AND (LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-                    "     OR LOWER(product_code) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-                    "     OR LOWER(description) LIKE LOWER(CONCAT('%', ?1, '%')) )",
-            nativeQuery = true)
-    Page<Product> searchProducts(String query, Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM product " +
-            "WHERE is_deleted = false " +
-            "AND category_id " +
-            "IN (SELECT id " +
-            "FROM category " +
-            "WHERE LOWER(name) = LOWER(?2)) " +
-            "AND (LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-            "     OR LOWER(product_code) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-            "     OR LOWER(description) LIKE LOWER(CONCAT('%', ?1, '%')) ) " +
-            "ORDER BY CASE WHEN position = 0 THEN 1 ELSE 0 END, position ASC, created_at ASC",
-            countQuery = "SELECT count(*) FROM product " +
-                    "WHERE is_deleted = false " +
-                    "AND category_id " +
-                    "IN (SELECT id " +
-                    "FROM category " +
-                    "WHERE LOWER(name) = LOWER(?2)) " +
-                    "AND (LOWER(name) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-                    "     OR LOWER(product_code) LIKE LOWER(CONCAT('%', ?1, '%')) " +
-                    "     OR LOWER(description) LIKE LOWER(CONCAT('%', ?1, '%')) )",
-            nativeQuery = true)
-    Page<Product> searchProductsByCategory(String query, String category, Pageable pageable);
 
     @Query(value = "SELECT COUNT(*) " +
             "FROM product " +
