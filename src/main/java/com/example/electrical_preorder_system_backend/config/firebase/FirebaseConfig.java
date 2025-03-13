4b727c5a981f000f14326033959de4c2e9c08e94
Nguyen Van Dai <dainvse170297@fpt.edu.vn>
2025-03-13 23:42:35 +0700

    update deploy workflow and firebase config

--- src/main/java/com/example/electrical_preorder_system_backend/config/firebase/FirebaseConfig.java
+++ src/main/java/com/example/electrical_preorder_system_backend/config/firebase/FirebaseConfig.java
@@ -4,22 +4,27 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
-@RequiredArgsConstructor
 public class FirebaseConfig {
+
+    @Value("${firebase.googleCredentials}")
+    private String firebaseCredentials;
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
+        ByteArrayInputStream credentialsStream = new ByteArrayInputStream(firebaseCredentials.getBytes());
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
+
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(
-                        new ClassPathResource("firebase-adminsdk.json").getInputStream()))
+                .setCredentials(credentials)
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
